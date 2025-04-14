114ddc007867d35eae3b81b469d894a905f04c12
yashicac <91489374+yashicac@users.noreply.github.com>
2025-04-14 16:36:31 -0400

    updated generate with better entropy

--- network/algos/rng.cpp
+++ network/algos/rng.cpp
@@ -1,16 +1,8 @@
 #include "rng.h"
-#include <ctime>
-#include <cstdint>
+#include <random>
+#include <chrono>
 
 long RNG::generate() {
-    static uint64_t seed = time(nullptr); //Using current time as seed
-
-    //Constants
-    const uint64_t a = 1664525;
-    const uint64_t c = 1013904223;
-    const uint64_t m = (1ULL << 32); // 2^32
-
-    seed = (a * seed + c) % m;
-
-    return static_cast<long>(seed);
+    std::mt19937_64 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+    return rng() % 1000000000;
 }
